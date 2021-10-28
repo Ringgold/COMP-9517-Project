b3e2a622ae0b63514c25b23b9a73fc9089b88707
Ringgold <ringgoldlin@yahoo.com>
2021-10-28 16:20:55 +0800

    10.28.2021: edge detect

--- task1.py
+++ task1.py
@@ -4,24 +4,42 @@ import matplotlib.pyplot as plt
 import math
 from pathlib import Path
 
+def to_binary_image(input_image: np.array, background_potion = 6):
+    input = input_image.copy()
+    meaningful_range = np.max(input) - np.min(input)
+    input = cv2.normalize(input, None, alpha=0, beta=meaningful_range, norm_type=cv2.NORM_MINMAX)
+    input[input >= meaningful_range/background_potion] = 255
+    input[input < meaningful_range/background_potion] = 0
+    return input
+
+def dilate_erode_reduce_noise(input_binary: np.array, kernel = (7,7), iteration = 11):
+    '''
+    Try Dilate and erode
+    1. Dilate the objects first to get rid of the noise inside each objects
+    2. erode the objects with same degree to decrease the size to original state
+    '''
+    input = input_binary.copy()
+    input = cv2.erode(input, kernel, iterations = iteration)
+    input = cv2.dilate(input, kernel, iterations = iteration)
+    return input
+
 def find_coutours_draw(input_gray: np.array):
     '''
     Find all of the coutours of the input image cell and draw the coutours
     '''
-    
-    # threshold = math.floor(pixel_norm_range/2)
-    # input_edges = cv2.Canny(input_gray, threshold, threshold)
-    # plt.title("input_edges")
-    # plt.imshow(input_edges, cmap="gray", vmin=0, vmax=pixel_norm_range)
-    # plt.show()
-
-    pixel_norm_range = np.max(input_gray) - np.min(input_gray)
-    input_norm = cv2.normalize(cell, None, alpha=0, beta=pixel_norm_range, norm_type=cv2.NORM_MINMAX)
-
-    ret, thresh = cv2.threshold(input_gray, math.floor(pixel_norm_range/3), np.max(input_norm), np.min(input_norm))
-    plt.title("input_gray")
-    plt.imshow(input_gray, cmap="gray", vmin=0, vmax=pixel_norm_range)
+    threshold = (np.max(input_gray) - np.min(input_gray))/2
+    input_edges = cv2.Canny(input_gray, threshold, threshold)
+    plt.title("input_edges")
+    plt.imshow(input_edges, cmap="gray")
     plt.show()
+
+    # pixel_norm_range = np.max(input_gray) - np.min(input_gray)
+    # input_norm = cv2.normalize(cell, None, alpha=0, beta=pixel_norm_range, norm_type=cv2.NORM_MINMAX)
+
+    # ret, thresh = cv2.threshold(input_gray, math.floor(pixel_norm_range/3), np.max(input_norm), np.min(input_norm))
+    # plt.title("input_gray")
+    # plt.imshow(input_gray, cmap="gray", vmin=0, vmax=pixel_norm_range)
+    # plt.show()
 
     # contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -68,6 +86,16 @@ if __name__ == "__main__":
     cell = cv2.imread(path_cell, cv2.IMREAD_GRAYSCALE)
     cell_seg = cv2.imread(path_seg, -1)
     cell_tra = cv2.imread(path_tra, -1)
+
+    # Turn into np array first
+    cell = np.array(cell)
+    # To binary
+    cell_binary = to_binary_image(cell)
+    # reduce noise
+    cell_binary_no_noise = dilate_erode_reduce_noise(cell_binary)
+    # get coutours
+    cell_binary_coutour = find_coutours_draw(cell_binary_no_noise)
+    
     '''
     Get meaningful range of the original image:
     From what I see, the original cells' images are just meaningful from 128 to 136 which is 8 numbers
@@ -78,8 +106,6 @@ if __name__ == "__main__":
     # plt.title("cell_norm")
     # plt.imshow(cell_norm, cmap="gray", vmin=0, vmax=meaningful_range)
     # plt.show()
-
-    find_coutours_draw(cell)
 
     # Check SEG
     # print(cell_seg.shape)
