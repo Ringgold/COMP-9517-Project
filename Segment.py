d5fee9fd90658067a4b61605aacbe2acf98c87a7
Persona-KamenRider-LQY <7829089@qq.com>
2021-10-29 00:50:45 +0800

    ADD Reconstruction Way

--- Segment.py
+++ Segment.py
@@ -7,8 +7,9 @@ from matplotlib import pyplot as plt
 from skimage.segmentation import  clear_border
 from skimage import  color
 from skimage.feature import peak_local_max
-from skimage.segmentation import watershed
+from skimage.segmentation import watershed, expand_labels
 from scipy import ndimage
+from skimage.morphology import reconstruction
 
 def convertImage(image, target_type_min, target_type_max):
     new_img = np.zeros_like(image)
@@ -17,6 +18,26 @@ def convertImage(image, target_type_min, target_type_max):
 
     return new_img
 
+def to_binary_image(input_image: np.array, background_potion = 6):
+    input = input_image.copy()
+    meaningful_range = np.max(input) - np.min(input)
+    print(meaningful_range)
+    input = cv2.normalize(input, None, alpha=0, beta=meaningful_range, norm_type=cv2.NORM_MINMAX)
+    input[input >= meaningful_range/background_potion] = 255
+    input[input < meaningful_range/background_potion] = 0
+    return input
+
+def dilate_erode_reduce_noise(input_binary: np.array, kernel=(3, 3), iteration=5):
+    '''
+    Try Dilate and erode
+    1. Dilate the objects first to get rid of the noise inside each objects
+    2. erode the objects with same degree to decrease the size to original state
+    '''
+    input = input_binary.copy()
+    input = cv2.dilate(input, kernel, iterations=iteration)
+    input = cv2.erode(input, kernel, iterations=iteration)
+
+    return input
 
 # def applyWatershed(image):
 #     thresh = cv2.GaussianBlur(image,(1,1),0)
@@ -57,47 +78,65 @@ def convertImage(image, target_type_min, target_type_max):
 #
 #     return mask,color_img
 #
-def doWatershed(image):
-    blur = cv2.GaussianBlur(image, (3,3), 0)
+# def doWatershed(image):
+#     blur = cv2.GaussianBlur(image, (3,3), 0)
+#
+#     # thresholding
+#     ret, th1 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_TRIANGLE)
+#     plt.imshow(th1)
+#     plt.show()
+#     # dist transform
+#     D = ndimage.distance_transform_edt(th1)
+#
+#     # markers
+#     localMax = peak_local_max(D, indices=False, min_distance=20, labels=th1)
+#     markers = ndimage.label(localMax, structure=np.ones((3, 3)))[0]
+#
+#     # apply watershed
+#     labels = watershed(-D, markers, mask=th1)
+#     print("[INFO] {} unique segments found".format(len(np.unique(labels)) - 1))
+#
+#     return labels
 
-    # thresholding
-    ret, th1 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_TRIANGLE)
-    plt.imshow(th1)
+
+
+def performSegmentation(image,iteration = 20):
+    img = image.copy()
+    kernel = np.ones((3,3),np.uint8)
+
+    dist_transform = cv2.distanceTransform(img, cv2.DIST_L2, 5)
+    _, seeds = cv2.threshold(dist_transform, 0.5 * dist_transform.max(), 255, 0)
+
+
+    res = cv2.connectedComponentsWithStats(np.uint8(seeds))
+    output = np.zeros_like(img)
+    mask_img = np.zeros_like(img)
+    mask_img = 255
+    labels = res[1]
+
+    for i in range(iteration):
+        labels = expand_labels(labels)
+        labels = cv2.bitwise_and(labels,mask_img,mask = img)
+
+    for label in np.unique(labels):
+        if(label == 0):
+            continue
+        output[labels == label] = 255
+
+    plt.imshow(seeds,cmap='gray')
     plt.show()
-    # dist transform
-    D = ndimage.distance_transform_edt(th1)
-
-    # markers
-    localMax = peak_local_max(D, indices=False, min_distance=20, labels=th1)
-    markers = ndimage.label(localMax, structure=np.ones((3, 3)))[0]
-
-    # apply watershed
-    labels = watershed(-D, markers, mask=th1)
-    print("[INFO] {} unique segments found".format(len(np.unique(labels)) - 1))
-
     return labels
 
 img_path = "./Sequences/01/t000.tif"
 print(img_path)
-image  = cv2.imread(img_path,-1)
-image = convertImage(image,0,255)
-#
-# mask,img = applyWatershed(image)
-# plt.imshow(mask)
-# plt.show()
-# _,contours,_= cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#
-# for contour in range(len(contours)):
-#     cv2.drawContours(img,contours,contour,np.random.randint(0,256,3).tolist(),5)
-#
-#
-# plt.imshow(img,cmap='gray')
-# plt.show()
+image  = cv2.imread(img_path,cv2.IMREAD_GRAYSCALE)
 
+b_img = to_binary_image(image)
 
+img_no_noise = dilate_erode_reduce_noise(b_img,iteration=10)
+labels = performSegmentation(img_no_noise)
 
-labels = doWatershed(image)
-color_image = cv2.cvtColor(image,cv2.COLOR_GRAY2RGB)
+color_image = cv2.cvtColor(img_no_noise,cv2.COLOR_GRAY2RGB)
 for label in np.unique(labels):
     if label == 0:
         continue
@@ -109,6 +148,6 @@ for label in np.unique(labels):
     # detect contours in the mask and grab the largest one
     _,cnts,_ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
-    cv2.drawContours(color_image, cnts, -1,np.random.randint(0,256,3).tolist(), 5)
+    cv2.drawContours(color_image, cnts, -1,np.random.randint(0,256,3).tolist(), 3)
 plt.imshow(color_image)
 plt.show()
